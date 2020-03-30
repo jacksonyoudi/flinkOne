74341504241495c7259157cbf32145b3e273e3e1
liangchangyou <liangchangyoujackson@gmail.com>
2020-03-30 08:03:06 +0800

    wc

--- src/main/scala/org/youdi/wc/WordCount.scala
+++ src/main/scala/org/youdi/wc/WordCount.scala
@@ -1,7 +1,21 @@
 package org.youdi.wc
 
+import org.apache.flink.api.scala._
+
+
 object WordCount {
   def main(args: Array[String]): Unit = {
+    // 创建一个批处理执行环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+
+    // 从文件中读取数据
+    val inputDataSet: DataSet[String] = env.readTextFile("./input")
+
+    // 分词
+    val agg: AggregateDataSet[(String, Int)] = inputDataSet.flatMap(_.split(" ")).map((_, 1)).groupBy(0).sum(1)
+
+    agg.print()
+
 
   }
 }
