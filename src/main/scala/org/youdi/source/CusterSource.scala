ef0a108e80a67475c0798d3b3c1f7489d3ae5bc7
liangchangyou <liangchangyoujackson@gmail.com>
2020-04-02 23:42:43 +0800

    custer

---
+++ src/main/scala/org/youdi/source/CusterSource.scala
@@ -0,0 +1,47 @@
+package org.youdi.source
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction
+import org.apache.flink.streaming.api.scala._
+
+import scala.util.Random
+
+object CusterSource {
+  def main(args: Array[String]): Unit = {
+    val streamEnv: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    streamEnv.setParallelism(1)
+
+    val stream: DataStream[StationLog] = streamEnv.addSource(new MyCuseterSource)
+
+    stream.print()
+    streamEnv.execute("stream")
+
+  }
+}
+
+
+class MyCuseterSource extends SourceFunction[StationLog] {
+  var flag: Boolean = true
+
+
+  override def run(ctx: SourceFunction.SourceContext[StationLog]): Unit = {
+
+    val random: Random = new Random()
+    while (flag) {
+      1.to(10).map(
+        i => {
+          val log: StationLog = StationLog("001" + random.nextGaussian() * 100, "186", "187", "busy", System.currentTimeMillis(), 0)
+          log
+        }
+      ).foreach(ctx.collect)
+
+      Thread.sleep(2000)
+
+    }
+  }
+
+
+  override def cancel(): Unit = {
+    flag = false
+  }
+}
